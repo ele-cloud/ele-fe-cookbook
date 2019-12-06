508ec4478e1da02440f6d80ad279cf32c1b9043a
Yong Yang <42476396+AngusYang9@users.noreply.github.com>
2019-12-06 16:41:16 +0800

    Update nav.js

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,6 +1,6 @@
 module.exports = [
   {text: '主页', link: '/'},
-  {text: '项目资源', link: 'http://119.3.215.227:3001/'},
+  {text: '项目资源', link: 'http://121.36.50.216:4002/'},
   {text: '📆禅道', link: 'http://10.1.3.241/zentaopms/www/index.php?m=my&f=index'},
   {text: '🍱加班订餐', link: 'https://meican.com/'},
 ]
